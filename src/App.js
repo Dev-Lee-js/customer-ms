ab142796654b5eb627160513607d2ca0b9cf23f9
Dev-Lee-js <whdtlr202@gmail.com>
2021-09-29 14:02:42 +0900

    고객 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -3,26 +3,53 @@ import './App.css';
 import Customer from './components/Customer';
 
 
-const customer = {
-  "id":1,
-  "image":"https://placeimg.com/64/64/any",
-  "name":"이종석",
-  "birthday":"980511",
-  "gender":"남자",
-  "job":"회사원"
-}
+const customers = [
+  {
+    "id":1,
+    "image":"https://placeimg.com/64/64/1",
+    "name":"이종석",
+    "birthday":"980511",
+    "gender":"남자",
+    "job":"회사원"
+  },
+  {
+    "id":2,
+    "image":"https://placeimg.com/64/64/2",
+    "name":"박종수",
+    "birthday":"123456",
+    "gender":"남자",
+    "job":"회사원"
+  },
+  {
+    "id":3,
+    "image":"https://placeimg.com/64/64/3",
+    "name":"이순신",
+    "birthday":"123456",
+    "gender":"남자",
+    "job":"회사원"
+  },
+]
 
 class App extends Component{
   render(){
     return(
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <>
+        {
+          customers.map(c=>{
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+              );
+          }) 
+        }
+      </>
     );
   }
 }

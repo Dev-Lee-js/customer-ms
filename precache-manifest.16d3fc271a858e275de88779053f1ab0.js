687aea8266e7b7c208833b5ffaefebb3fc72806e
Dev-Lee-js <whdtlr202@gmail.com>
2021-09-29 20:30:18 +0900

    Updates

---
+++ precache-manifest.16d3fc271a858e275de88779053f1ab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ee9cf7d46777d5f6ba2",
+    "url": "/customer-ms/static/css/main.5e4752fe.chunk.css"
+  },
+  {
+    "revision": "3ee9cf7d46777d5f6ba2",
+    "url": "/customer-ms/static/js/main.3ee9cf7d.chunk.js"
+  },
+  {
+    "revision": "e6d5ac5f81244617d7ea",
+    "url": "/customer-ms/static/js/1.e6d5ac5f.chunk.js"
+  },
+  {
+    "revision": "e05d0fdb481bea4ed76e",
+    "url": "/customer-ms/static/js/runtime~main.e05d0fdb.js"
+  },
+  {
+    "revision": "3cb1f6651ecb8e91cc95cedf497812d8",
+    "url": "/customer-ms/index.html"
+  }
+];

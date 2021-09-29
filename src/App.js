9c1845f9dcd3aab8babb8535badb9699e837e602
Dev-Lee-js <whdtlr202@gmail.com>
2021-09-29 13:02:59 +0900

    고객 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React,  { Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const customer = {
+  "id":1,
+  "image":"https://placeimg.com/64/64/any",
+  "name":"이종석",
+  "birthday":"980511",
+  "gender":"남자",
+  "job":"회사원"
+}
+
+class App extends Component{
+  render(){
+    return(
+      <Customer
+        id={customer.id}
+        image={customer.image}
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
